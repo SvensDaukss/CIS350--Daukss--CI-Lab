b39914022fe24926bdeb87d5df8de75f5a4bb920
svensdaukss <svensdaukss@gmail.com>
2023-12-06 12:09:11 -0500

    added functions and tests for calc_sum and calc_product

--- math_functions.py
+++ math_functions.py
@@ -14,8 +14,18 @@ def divide_numbers(a, b):
     return a / b
 
 
+def calc_sum(a, b, c):
+    return a + b + c
+
+
+def calc_product(a, b, c):
+    return a * b * c
+
+
 if __name__ == "__main__":
     print("Adding:", add_numbers(2, 4))
     print("Subtracting:", subtract_numbers(9, 2))
     print("Multiplying:", multiply_numbers(2, 4))
-    print("Adding:", subtract_numbers(2, 4))
+    print("Adding:", divide_numbers(2, 4))
+    print("Sum:", calc_sum(2, 4, 7))
+    print("Product:", calc_sum(2, 4, 7))
--- test_math_functions.py
+++ test_math_functions.py
@@ -24,3 +24,13 @@ def test_calc_multiply_pass():
 def test_calc_divide():
     output = divide_numbers(10, 2)
     assert output == 5
+
+
+def test_calc_sum():
+    output = calc_sum(25, 20, 29)
+    assert output == 74
+
+
+def test_calc_product():
+    output = calc_product(10, 20, 27)
+    assert output == 5400
